3471f1515651d83895d023533bb6fa1a750f2727
@r0f33 <jiske.nicolson@gmail.com>
2024-05-27 19:16:09 +0930

    added seeds and routes for basic users table

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const welcomeRoutes = require("./welcomeRoutes");
+const loginRoutes = require("./loginRoutes");
+const apiRoutes = require("./api");
 
 router.use("/", welcomeRoutes);
+router.use("/login", loginRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
