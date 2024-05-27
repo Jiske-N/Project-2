d2df2650f0e5582efa7260861ba09fecc00f915b
Gayle McClure <gaylekdennison@gmail.com>
2024-05-27 19:33:00 +1000

    added task routes, board and popup layouts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const welcomeRoutes = require('./welcomeRoutes');
 
-router.use('/', welcomeRoutes);
+const taskRoutes = require('./taskRoutes');
+
+router.use('/tasks', taskRoutes);
+
 
 module.exports = router;
-
---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const welcomeRoutes = require('./welcomeRoutes');
+const apiRoutes = require('./api')
+
+router.use('/', welcomeRoutes);
+router.use('/api', apiRoutes)
+
+module.exports = router;
+
+
